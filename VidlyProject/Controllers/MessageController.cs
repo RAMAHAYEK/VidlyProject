9d112ea5522a5407b2660a2daf00e3f951f0ae8f
RAMAHK\Rama <ramahayekit@gmail.com>
2018-03-10 15:40:30 +0200

    add contact us page

---
+++ VidlyProject/Controllers/MessageController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyProject.Models;
+
+namespace VidlyProject.Controllers
+{
+    public class MessageController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public MessageController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Message
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Save(Message message)
+        {
+            if(message.Id == 0)
+            {
+                _context.Messages.Add(message);
+                _context.SaveChanges();
+            }
+            return Json( "saved successfully .");
+          
+        }
+    }
+}
